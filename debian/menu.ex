35d52495a3beb33c0752c9fb1100a272f701220f
Paul Klos <kolab@klos2day.nl>
2012-10-30 19:24:12 +0100

    Moved /cli/telemetry/* from kolab-cli to kolab-telemetry Removed debian/*.ex

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pykolab):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pykolab" command="/usr/bin/pykolab"
--- debian/pykolab.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pykolab package
-#
-0 4	* * *	root	[ -x /usr/bin/pykolab_maintenance ] && /usr/bin/pykolab_maintenance
